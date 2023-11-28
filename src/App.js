e3a3c042b4e1f6e1ea4e2ad2782a1a7643a94c4f
hyeonjuuu <janghyeonjuu@gmail.com>
2023-11-28 10:14:57 +0900

    valtio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
 import "./App.css";
 import Page from "./components/Page";
+import { proxy, useSnapshot } from "valtio";
+import { darkState } from "./valtio";
+
+// const darkState = proxy({ isDark: false });
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
-  return <Page isDark={isDark} toggleThema={() => setIsDark(!isDark)} />;
+  const isDark = useSnapshot(darkState).isDark; //Read-only 값만 가져옴.
+
+  return (
+    <Page isDark={isDark} toggleThema={() => (darkState.isDark = !isDark)} />
+  );
 }
 
 export default App;
